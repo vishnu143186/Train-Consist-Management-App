4aa85626f7783d6f1070f6a5ccc2d2e706703777
vishnu143186 <mm1475@srmist.edu.in>
2026-04-07 10:30:54 +0530

    Map Bogie to Capacity (HashMap)

--- app/src/TrainConsistManagementApp.java
+++ app/src/TrainConsistManagementApp.java
@@ -1,16 +1,16 @@
-import java.util.LinkedHashSet;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TrainConsistManagementApp {
     public static void main(String[] args) {
-        LinkedHashSet<String> formation = new LinkedHashSet<>();
+        HashMap<String, Integer> bogieCapacities = new HashMap<>();
 
-        formation.add("Engine");
-        formation.add("Sleeper");
-        formation.add("Cargo");
-        formation.add("Guard");
+        bogieCapacities.put("Sleeper", 72);
+        bogieCapacities.put("AC Chair", 56);
+        bogieCapacities.put("First Class", 24);
 
-        formation.add("Sleeper");
-
-        System.out.println("Final Formation Order: " + formation);
+        for (Map.Entry<String, Integer> entry : bogieCapacities.entrySet()) {
+            System.out.println("Bogie: " + entry.getKey() + " | Capacity: " + entry.getValue());
+        }
     }
 }
